f305030019ac8a15779fdfcacf3d4d4d5a23ce62
Ggaming5005 <ggaming5005@gmail.com>
2024-11-16 11:45:15 -0800

    made it

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Translator from "./components/Translator";
+import "./styles/Translator.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Unicode UTF Translator</h1>
+      <Translator />
+    </div>
+  );
+}
+
+export default App;
